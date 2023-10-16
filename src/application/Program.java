a92be9a89b7574500c6c32cd7e7818621d019068
Victor Prudente <victorprudt@gmail.com>
2023-10-16 20:22:13 -0300

    Create Seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        System.out.println(department);
+        Department books = new Department(1, "Books");
+
+        Seller seller1 = new Seller(21, "Seller 1", "seller@gmail.com", new Date(), 3000.0, books);
+
+        System.out.println(seller1);
+        System.out.println(books);
+
     }
 }
